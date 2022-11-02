9ffda3c4433c6cfaf8b83b4abeea75f6a6b1f9b8
LeOzInNDF <leo_albano@hotmail.com>
2022-11-02 15:21:51 -0300

    att Event Listener em imagens

--- app/src/main/java/br/com/eletriccarapp/presentation/CalcularAutonimiaActivity.kt
+++ app/src/main/java/br/com/eletriccarapp/presentation/CalcularAutonimiaActivity.kt
@@ -1,14 +1,55 @@
 package br.com.eletriccarapp.presentation
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import br.com.eletriccarapp.R
 
 class CalcularAutonimiaActivity : AppCompatActivity(){
 
+    lateinit var preco: EditText
+    lateinit var kmPercorrido: EditText
+    lateinit var btnCalcular: Button
+    lateinit var resultado: TextView
+    lateinit var btnCloser: ImageView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calcular_autonomia)
+        setupView()
+        setupListners()
     }
+
+    fun setupView() {
+        preco = findViewById(R.id.et_preco_kwh)
+        kmPercorrido = findViewById(R.id.et_km_percorrido)
+        resultado = findViewById(R.id.tv_resultado)
+        btnCalcular = findViewById(R.id.btn_calcular)
+        btnCloser = findViewById(R.id.iv_close)
+    }
+    fun setupListners() {
+        btnCalcular.setOnClickListener {
+            calcular()
+        }
+        btnCloser.setOnClickListener {
+            finish()
+        }
+    }
+
+    fun calcular() {
+        val preco = preco.text.toString().toFloat()
+        val km = kmPercorrido.text.toString().toFloat()
+
+        val result = preco / km
+
+        resultado.text = result.toString()
+
+    }
+
 
 }
