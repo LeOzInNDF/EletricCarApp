6a064076ac6c47acc03b628d80089273f70608e3
LeOzInNDF <leo_albano@hotmail.com>
2022-11-02 12:53:14 -0300

    Commit para versionamento do codigo

---
+++ README.md
@@ -0,0 +1,15 @@
+# Aplicativo de Carros Elétricos 
+
+## Sobre o Autor
+Oi, sou o Leonardo! Estou construindo esse aplicativo Android utilizando Kotlin. 
+
+
+
+
+## <br />Descrição
+Módulo - Noções Básicas do Android com Kotlin 
+1. Princípios de Interface de Usuário e Layouts Android
+2. Manipulando Entradas do Usuário em Apps Android
+3. Fundamentos de Multiscreen em Apps Android
+4. Noções Básicas de Rede em Apps Android com Kotlin
+5. Armazenamento de Dados Essencial em Apps Android com Kotlin
---
+++ app/src/main/java/br/com/eletriccarapp/MainActivity.kt
@@ -0,0 +1,48 @@
+package br.com.eletriccarapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var preco: EditText
+    lateinit var btnCalcular: Button
+    lateinit var kmPercorrido: EditText
+    lateinit var resultado: TextView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupView()
+        setupListners()
+    }
+
+    fun setupView() {
+        preco = findViewById(R.id.et_preco_kwh)
+        kmPercorrido = findViewById(R.id.et_km_percorrido)
+        btnCalcular = findViewById(R.id.btn_calcular)
+        resultado = findViewById(R.id.tv_resultado)
+
+    }
+
+    fun setupListners() {
+        btnCalcular.setOnClickListener {
+            calcular()
+        }
+    }
+
+    fun calcular() {
+        val preco = preco.text.toString().toFloat()
+        val km = kmPercorrido.text.toString().toFloat()
+
+        val result = preco / km
+
+        resultado.text = result.toString()
+
+    }
+}
