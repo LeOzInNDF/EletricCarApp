ad8099d4649ca6efaf13803c254cbade805de32a
LeOzInNDF <leo_albano@hotmail.com>
2022-11-05 13:04:52 -0300

    applying Fragment

---
+++ app/src/main/java/br/com/eletriccarapp/ui/CarFragment.kt
@@ -0,0 +1,19 @@
+package br.com.eletriccarapp.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import br.com.eletriccarapp.R
+
+class CarFragment : Fragment {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.car_fragment, container, false)
+    }
+}
